895fa6d49a3f8c13ecb42c39a82e33f19c4ce899
Mitchell Richters <mjr4077au@gmail.com>
2024-12-08 02:30:59 +1100

    - Add in web utilities from PSAppDeployToolkit's graveyard, we'll need these soon.
    https://github.com/PSAppDeployToolkit/PSAppDeployToolkit/commit/416fab3377cf66fc5218ff62656cf4fb336a5a0c

---
+++ src/PSAppDeployToolkit.WinGet/Private/Get-ADTRedirectedUri.ps1
@@ -0,0 +1,111 @@
+﻿#-----------------------------------------------------------------------------
+#
+# MARK: Get-ADTRedirectedUri
+#
+#-----------------------------------------------------------------------------
+
+function Get-ADTRedirectedUri
+{
+    <#
+    .SYNOPSIS
+        Returns the resolved URI from the provided permalink.
+
+    .DESCRIPTION
+        This function gets the resolved/redirected URI from the provided input and returns it to the caller.
+
+    .PARAMETER Uri
+        The URL that requires redirection resolution.
+
+    .PARAMETER Headers
+        Any headers that need to be provided for URI redirection resolution.
+
+    .INPUTS
+        None
+
+        You cannot pipe objects to this function.
+
+    .OUTPUTS
+        System.Uri
+
+        Get-ADTRedirectedUri returns a Uri of the resolved/redirected URI.
+
+    .EXAMPLE
+        Get-ADTRedirectedUri -Uri https://aka.ms/getwinget
+
+        Returns the absolute URI for the specified short link, e.g. https://github.com/microsoft/winget-cli/releases/download/v1.8.1911/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
+    .LINK
+        https://github.com/mjr4077au/PSAppDeployToolkit.WinGet
+    #>
+
+    [CmdletBinding()]
+    [OutputType([System.Uri])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({
+                if (![System.Uri]::IsWellFormedUriString($_.AbsoluteUri, [System.UriKind]::Absolute))
+                {
+                    $PSCmdlet.ThrowTerminatingError((New-ADTValidateScriptErrorRecord -ParameterName Uri -ProvidedValue $_ -ExceptionMessage 'The specified input is not a valid Uri.'))
+                }
+                return !!$_
+            })]
+        [System.Uri]$Uri,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [System.Collections.IDictionary]$Headers = @{ Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' }
+    )
+
+    begin
+    {
+        # Initialize function.
+        Initialize-ADTFunction -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    }
+
+    process
+    {
+        try
+        {
+            try
+            {
+                # Create web request.
+                Write-ADTLogEntry -Message "Retrieving the redirected URI for [$Uri]."
+                $webReq = [System.Net.WebRequest]::Create($Uri)
+                $webReq.AllowAutoRedirect = $false
+                $Headers.GetEnumerator() | & { process { $webReq.($_.Key) = $_.Value } }
+
+                # Get a response and close it out.
+                $reqRes = $webReq.GetResponse()
+                $resLoc = $reqRes.GetResponseHeader('Location')
+                $reqRes.Close()
+
+                # If $resLoc is empty, return the provided URI so something is returned to the caller.
+                if (![System.String]::IsNullOrWhiteSpace($resLoc))
+                {
+                    $Uri = $resLoc
+                }
+
+                # Return the redirected URI to the caller.
+                Write-ADTLogEntry -Message "Retrieved redireted URI [$Uri] from the provided input."
+                return $Uri
+            }
+            catch
+            {
+                # Re-writing the ErrorRecord with Write-Error ensures the correct PositionMessage is used.
+                Write-Error -ErrorRecord $_
+            }
+        }
+        catch
+        {
+            # Process the caught error, log it and throw depending on the specified ErrorAction.
+            Invoke-ADTFunctionErrorHandler -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState -ErrorRecord $_ -LogMessage "Failed to determine the redirected URI for [$Uri]."
+        }
+    }
+
+    end
+    {
+        # Finalize function.
+        Complete-ADTFunction -Cmdlet $PSCmdlet
+    }
+}
---
+++ src/PSAppDeployToolkit.WinGet/Private/Get-ADTUriFileName.ps1
@@ -0,0 +1,111 @@
+﻿#-----------------------------------------------------------------------------
+#
+# MARK: Get-ADTUriFileName
+#
+#-----------------------------------------------------------------------------
+
+function Get-ADTUriFileName
+{
+    <#
+    .SYNOPSIS
+        Returns the filename of the provided URI.
+
+    .DESCRIPTION
+        This function gets the filename of the provided URI from the provided input and returns it to the caller.
+
+    .PARAMETER Uri
+        The URL that to retrieve the filename from.
+
+    .INPUTS
+        None
+
+        You cannot pipe objects to this function.
+
+    .OUTPUTS
+        System.String
+
+        Get-ADTUriFileName returns a string value of the URI's filename.
+
+    .EXAMPLE
+        Get-ADTUriFileName -Uri https://aka.ms/getwinget
+
+        Returns the filename for the specified URI, redirected or otherwise. e.g. Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
+    .LINK
+        https://github.com/mjr4077au/PSAppDeployToolkit.WinGet
+    #>
+
+    [CmdletBinding()]
+    [OutputType([System.String])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({
+                if (![System.Uri]::IsWellFormedUriString($_.AbsoluteUri, [System.UriKind]::Absolute))
+                {
+                    $PSCmdlet.ThrowTerminatingError((New-ADTValidateScriptErrorRecord -ParameterName Uri -ProvidedValue $_ -ExceptionMessage 'The specified input is not a valid Uri.'))
+                }
+                return !!$_
+            })]
+        [System.Uri]$Uri
+    )
+
+    begin
+    {
+        # Initialize function.
+        Initialize-ADTFunction -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    }
+
+    process
+    {
+        try
+        {
+            try
+            {
+                # Re-write the URI to factor in any redirections.
+                Write-ADTLogEntry -Message "Retrieving the file name for URI [$Uri]."
+                $Uri = Get-ADTRedirectedUri -Uri $Uri
+
+                # Create web request.
+                $webReq = [System.Net.WebRequest]::Create($Uri)
+                $webReq.AllowAutoRedirect = $false
+                $webReq.Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
+
+                # Get a response and close it out.
+                $reqRes = $webReq.GetResponse()
+                $resCnt = $reqRes.GetResponseHeader('Content-Disposition')
+                $reqRes.Close()
+
+                # If $resCnt is empty, the provided URI likely has the filename in it.
+                $filename = if (!$resCnt.Contains('filename'))
+                {
+                    Remove-ADTInvalidFileNameChars -Name $Uri.ToString().Split('/')[-1]
+                }
+                else
+                {
+                    Remove-ADTInvalidFileNameChars -Name $resCnt.Split(';').Trim().Where({ $_.StartsWith('filename=') }).Split('=')[-1]
+                }
+
+                # Return the determined filename to the caller.
+                Write-ADTLogEntry -Message "Resolved filename [$filename] from the provided URI."
+                return $filename
+            }
+            catch
+            {
+                # Re-writing the ErrorRecord with Write-Error ensures the correct PositionMessage is used.
+                Write-Error -ErrorRecord $_
+            }
+        }
+        catch
+        {
+            # Process the caught error, log it and throw depending on the specified ErrorAction.
+            Invoke-ADTFunctionErrorHandler -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState -ErrorRecord $_ -LogMessage "Failed to determine the filename for URI [$Uri]."
+        }
+    }
+
+    end
+    {
+        # Finalize function.
+        Complete-ADTFunction -Cmdlet $PSCmdlet
+    }
+}

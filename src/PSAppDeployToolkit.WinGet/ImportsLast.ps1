ef8245308cdadbe97dc080773464a1696d776e2c
Mitch Richters <48643140+mjr4077au@users.noreply.github.com>
2025-07-12 18:11:48 +1000

    - Update minimum WinGet version to v1.10.390.

--- src/PSAppDeployToolkit.WinGet/ImportsLast.ps1
+++ src/PSAppDeployToolkit.WinGet/ImportsLast.ps1
@@ -18,7 +18,7 @@ try
     try
     {
         New-Variable -Name ADT -Option Constant -Value ([pscustomobject]@{
-                WinGetMinVersion = [System.Version]::new(1, 7, 10582)
+                WinGetMinVersion = [System.Version]::new(1, 10, 390)
                 RunningAsSystem = $currentWindowsIdentity.User.IsWellKnown([System.Security.Principal.WellKnownSidType]::LocalSystemSid)
                 RunningAsAdmin = Test-ADTCallerIsAdmin
                 SystemArchitecture = [System.Runtime.InteropServices.RuntimeInformation, mscorlib, Version = 4.0.0.0, Culture = neutral, PublicKeyToken = b77a5c561934e089]::OSArchitecture.ToString().ToLower()
